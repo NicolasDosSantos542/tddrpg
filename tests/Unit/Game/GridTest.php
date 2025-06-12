6d5681352c5ef5f5232f599039bb88d67411fd1b
dxmprofuse <administrateur@dxmprofuse.fr>
2025-06-12 14:01:36 +0200

    Add cell management methods to Grid class and corresponding tests

--- tests/Unit/Game/GridTest.php
+++ tests/Unit/Game/GridTest.php
@@ -29,5 +29,49 @@ class GridTest extends TestCase
         }
     }
 
+    public function testCheckEmptyCell(): void
+    {
 
+        $grid = new Grid(5, 5);
+
+        $cell = $grid->getCell(1, 1);
+        $this->assertEquals('empty', $cell);
+
+    }
+
+    public function testFillCell(): void
+    {
+        $grid = new Grid(5, 5);
+        $content = ['' => ''];
+        $cell = $grid->changeCell(1, 1, $content);
+        $this->assertEquals($content, $cell->content);
+    }
+
+    public function testgetInvalidCell(): void
+    {
+
+        $grid = new Grid(1, 1);
+
+        $cell = $grid->getCell(2, 1);
+        $this->assertNull($cell);
+    }
+
+    public function testfillCellChangeGrid(): void
+    {
+        $grid = new Grid(5, 5);
+        $content = ['' => ''];
+        $grid->changeCell(1, 1, $content);
+        $this->assertEquals($content, $grid->getCell(1, 1)->content);
+    }
+
+    public function testMoveCellContent(): void
+    {
+        $grid = new Grid(2, 2);
+        $content = ['' => ''];
+        $grid->changeCell(1, 1, $content);
+        $grid->moveContent(1, 1, 2, 2);
+        $this->assertEquals($content, $grid->getCell(2, 2)->content);
+        $this->assertEquals("empty", $grid->getCell(1, 1)->content);
+
+    }
 }
